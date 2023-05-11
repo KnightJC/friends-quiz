9b5a4e7ebbba145108d729687128ad723cb7e78f
KnightJC <>
2023-05-11 18:40:52 +0000

    Add html structure again to display

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,14 @@
-const quiz = [
-    {
-      q:'What is the name of the coffee shop the friends often hang out at?',
-      options:['Central Perk','Tavern','Spice Lounge','Coffee Bean'],
-      answer: 0
-    },
-    {
-        q:'What is the name of the monkey?',
-        options:['John','Marcel','Charlotte','Bean'],
-        answer: 1
-      },
-      {
-        q:'What is the name of the spin-off show featuring Joey?',
-        options:['Terminator','Mac N Cheese','Joey','Cows and Indians'],
-        answer: 2
-      },
-      {
-        q:'Who does Ross have an on-and-off relationship with throughout the series?',
-        options:['Karen','Monica','Carol','Rachel'],
-        answer: 3
-      },
-      {
-        q:'What is Monicas job?',
-        options:['Chef','Cleaner','Taxi Driver','Broker'],
-        answer: 0
-      }
-    ]
+document.addEventListener("DOMContentLoaded", function(){
+    let buttons = document.getElementsByTagName("buttons");
+    
+    for (let button of buttons){
+        button.addEventListener("click", function(){
+            if(this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+        })
+    }
+})
