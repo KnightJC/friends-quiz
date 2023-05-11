ed939f07d5b64974bf2ea672fcfc453fca40eb4e
KnightJC <>
2023-05-11 17:13:58 +0000

    Added Java to page

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+const quiz = [
+    {
+      q:'What is the name of the coffee shop the friends often hang out at?',
+      options:['Central Perk','Tavern','Spice Lounge','Coffee Bean'],
+      answer: 0
+    },
+    {
+        q:'What is the name of the monkey?',
+        options:['John','Marcel','Charlotte','Bean'],
+        answer: 1
+      },
+      {
+        q:'What is the name of the spin-off show featuring Joey?',
+        options:['Terminator','Mac N Cheese','Joey','Cows and Indians'],
+        answer: 2
+      },
+      {
+        q:'Who does Ross have an on-and-off relationship with throughout the series?',
+        options:['Karen','Monica','Carol','Rachel'],
+        answer: 3
+      },
+      {
+        q:'What is Monicas job?',
+        options:['Chef','Cleaner','Taxi Driver','Broker'],
+        answer: 0
+      }
+    ]
